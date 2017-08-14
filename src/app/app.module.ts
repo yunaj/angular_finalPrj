ef9eb842db31d3d4727bd02f5e66e1baab9928aa
terayuna <terayuna@gmail.com>
2017-08-14 15:53:09 +0900

    기능/핵심 모듈 분리

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { ScmMainModule } from './scm-main/scm-main.module';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    FooterComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+
+    //app modules
+    ScmMainModule,
+    ProductModule,
+    CategoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scm-main/scm-main.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  exports: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ]
+})
+export class ScmMainModule { }
