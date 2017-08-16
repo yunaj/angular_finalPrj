70c9100203118c35a27cbd0a21161776ad63a142
terayuna <terayuna@gmail.com>
2017-08-16 15:04:03 +0900

    firebase 연동

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 
 import { ScmMainModule } from './scm-main/scm-main.module';
 import { ProductModule } from './product/product.module';
@@ -15,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
     BrowserModule,
     FormsModule,
     HttpModule,
@@ -23,7 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
     ScmMainModule,
     ProductModule,
     CategoryModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDQYcx-1--OVgJElxnUnpT4ICpL7jrFfbQ",
+    authDomain: "angular-ex-9432b.firebaseapp.com",
+    databaseURL: "https://angular-ex-9432b.firebaseio.com",
+    projectId: "angular-ex-9432b",
+    storageBucket: "angular-ex-9432b.appspot.com",
+    messagingSenderId: "102422323051"
+  }
 };
